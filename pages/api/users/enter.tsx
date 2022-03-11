5a216c5bc978d7e6702ddccb878ce5d2ff4a4962
heekang2271 <hk.shin2271@gmail.com>
2022-03-11 14:20:14 +0900

    refactoring

---
+++ pages/api/users/enter.tsx
@@ -0,0 +1,12 @@
+import withHandler from '@libs/server/withHandler';
+import { NextApiRequest, NextApiResponse } from 'next';
+import client from '@libs/server/client';
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log(req.body);
+  res.json({
+    ok: true,
+  });
+}
+
+export default withHandler('POST', handler);
